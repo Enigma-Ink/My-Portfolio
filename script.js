68fc6b3f5c53746e2076693e4d4af37a0e95e8d7
Mako Leonor <ewananobato@gmail.com>
2024-08-12 22:33:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".loop", {
+    strings: ["Frontend Developer", "Security Research Analyst", "Frontend Developer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+});
